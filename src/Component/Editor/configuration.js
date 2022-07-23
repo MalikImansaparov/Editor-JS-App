51eaa5af9332d0e905d508bc08633809c669cd75
imansaparov <malikimansaparov@gmail.com>
2022-07-23 19:38:33 +0600

    feat: implement language internationalzation feature for UI

--- src/Component/Editor/configuration.js
+++ src/Component/Editor/configuration.js
@@ -28,12 +28,6 @@ const Configuration = () => {
         },
       },
 
-      // variable: {
-      //   config: {
-      //     placeholder: 'Enter something',
-      //   },
-      // },
-
       variable: {
         class: CustomVariable,
         inlineToolbar: true,
@@ -101,6 +95,113 @@ const Configuration = () => {
       },
     },
 
+    /**
+     * Internationalzation config
+     */
+    i18n: {
+      messages: {
+        ui: {
+          blockTunes: {
+            toggler: {
+              'Click to tune': 'Нажмите, чтобы настроить',
+              'or drag to move': 'или перетащите',
+            },
+          },
+          inlineToolbar: {
+            converter: {
+              'Convert to': 'Конвертировать в',
+            },
+          },
+          toolbar: {
+            toolbox: {
+              Add: 'Добавить',
+            },
+          },
+        },
+
+        /**
+         * Section for translation Tool Names: both block and inline tools
+         */
+        toolNames: {
+          Text: 'Параграф',
+          Heading: 'Заголовок',
+          List: 'Список',
+          Warning: 'Примечание',
+          Checklist: 'Чеклист',
+          Quote: 'Цитата',
+          Code: 'Код',
+          Delimiter: 'Разделитель',
+          'Raw HTML': 'HTML-фрагмент',
+          Table: 'Таблица',
+          Link: 'Ссылка',
+          Marker: 'Маркер',
+          Bold: 'Полужирный',
+          Italic: 'Курсив',
+          InlineCode: 'Моноширинный',
+          Image: 'Изображения',
+          Ordered: 'Упорядоченный',
+          Unordered: 'Неупорядоченный',
+          Filter: 'Филтрация',
+          CustomVariable: 'Переменный',
+        },
+
+        /**
+         * Section for passing translations to the external tools classes
+         */
+        tools: {
+          /**
+           * Each subsection is the i18n dictionary that will be passed to the corresponded plugin
+           * The name of a plugin should be equal the name you specify in the 'tool' section for that plugin
+           */
+          warning: {
+            // <-- 'Warning' tool will accept this dictionary section
+            Title: 'Название',
+            Message: 'Сообщение',
+          },
+
+          /**
+           * Link is the internal Inline Tool
+           */
+          link: {
+            'Add a link': 'Вставьте ссылку',
+          },
+          /**
+           * The "stub" is an internal block tool, used to fit blocks that does not have the corresponded plugin
+           */
+          stub: {
+            'The block can not be displayed correctly.':
+              'Блок не может быть отображен',
+          },
+        },
+
+        /**
+         * Section allows to translate Block Tunes
+         */
+        blockTunes: {
+          /**
+           * Each subsection is the i18n dictionary that will be passed to the corresponded Block Tune plugin
+           * The name of a plugin should be equal the name you specify in the 'tunes' section for that plugin
+           *
+           * Also, there are few internal block tunes: "delete", "moveUp" and "moveDown"
+           */
+          ordered: {
+            Ordered: 'Упорядоченный',
+          },
+          unordered: {
+            Unordered: 'Неупорядоченный',
+          },
+          delete: {
+            Delete: 'Удалить',
+          },
+          moveUp: {
+            'Move up': 'Переместить вверх',
+          },
+          moveDown: {
+            'Move down': 'Переместить вниз',
+          },
+        },
+      },
+    },
 
     data: {
       blocks: [
